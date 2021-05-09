3086bbdc58d96f4c61aad2ec55e1b51794f78714
morzh <morzh80@mail.ru>
2021-05-09 06:00:44 +0300

    starting to implement zoom in face detection page

--- lib/edit_choice_page.dart
+++ lib/edit_choice_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:vector_math/vector_math_64.dart' show Vector3;
 import 'package:photo_view/photo_view.dart';
-// import ‘package:mlkit/mlkit.dart’;
 
 
 List<AssetImage> _editChoiceButtons = [
--- lib/face_detection_page.dart
+++ lib/face_detection_page.dart
@@ -1,6 +1,3 @@
-
-
-import 'package:flutter/foundation.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 
