32555a67717a61a69b38f09d2f366941e8d79062
morzh <morzh80@mail.ru>
2021-05-30 00:56:29 +0300

    face detection page working, need to center image and check differetn image aspect ratios

--- lib/face_detection_page.dart
+++ lib/face_detection_page.dart
@@ -2,12 +2,15 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:typed_data';
 import 'dart:math';
+import 'dart:core';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:face_project_app/edit_choice_page.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_editor/image_editor.dart';
+import 'package:image_size_getter/image_size_getter.dart';
+import 'package:image_size_getter/file_input.dart';
 
 class FaceDetectionPage extends StatefulWidget {
   final File imageFile;
@@ -22,11 +25,15 @@ class _FaceDetectionPage extends State<FaceDetectionPage> {
       FaceDetectorOptions(mode: FaceDetectorMode.accurate));
   late File _fileBBoxFace;
   final _rng = Random();
+  int _imageWidth  = -1;
+  int _imageHeight = -1;
+  double _scale = -1.0;
+  int _imageLongestSide = -1;
 
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder(
-        future: Future.wait([detectFaces(), loadImage()]),
+        future: Future.wait([detectFaces(), loadImage(), getIamgeSize()]),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           return snapshot.hasData ? new Scaffold(
             body: snapshot.data[0].length == 0
@@ -40,27 +47,28 @@ class _FaceDetectionPage extends State<FaceDetectionPage> {
                 )
               )
             )
-            :Center(
-              child: InteractiveViewer(
-                  minScale: 0.45,
-                  maxScale: 3.0,
+            :InteractiveViewer(
+                  minScale: 0.15,
+                  maxScale: 5.0,
                     child: Stack(
                       fit: StackFit.expand,
                       children: <Widget>[
-                        Image.memory(snapshot.data[1]),
-                        for (var face in snapshot.data[0] )
+                        Image.memory(
+                            snapshot.data[1],
+                          alignment: Alignment.topLeft,
+                        ),
+                        for (var face in snapshot.data[0])
                           Positioned(
-                            top: 0.41*face.boundingBox.top,
-                            left: 0.41*face.boundingBox.left,
-                            width: 0.41*face.boundingBox.width,
-                            height: 0.41*face.boundingBox.height,
+                            top: (MediaQuery.of(context).size.width / snapshot.data[2]).clamp(0.01, 1.0) * face.boundingBox.top,
+                            left: (MediaQuery.of(context).size.width / snapshot.data[2]).clamp(0.01, 1.0) * face.boundingBox.left,
+                            width: (MediaQuery.of(context).size.width / snapshot.data[2]).clamp(0.01, 1.0) * face.boundingBox.width,
+                            height: (MediaQuery.of(context).size.width / snapshot.data[2]).clamp(0.01, 1.0)  * face.boundingBox.height,
                             child: GestureDetector(
                                 onTap: () async {
                                     final String random = (_rng.nextInt(1000000)).toString();
                                     final Uint8List data = await _cropImage(snapshot.data[1], face.boundingBox);
                                     final String tempPath = (await getTemporaryDirectory()).path;
                                     final File dataFile = File('$tempPath/$random.jpg');
-                                    // final File dataFile = File('$tempPath/tempFaceImage.jpg');
                                     if (dataFile.existsSync()) { dataFile.deleteSync(); }
                                     dataFile.writeAsBytesSync(data);
                                     // print(dataFile.path);
@@ -86,7 +94,7 @@ class _FaceDetectionPage extends State<FaceDetectionPage> {
                       ],
                     ),
                   ),
-                ),
+                // ),
           )
             :
           new Center(
@@ -105,22 +113,38 @@ class _FaceDetectionPage extends State<FaceDetectionPage> {
   }
 
   Future detectFaces() async {
-    final FirebaseVisionImage firebaseImage = FirebaseVisionImage.fromFile(
-        widget.imageFile);
-
-    final List<Face> detectedFaces = await faceDetector.processImage(
-        firebaseImage);
+    final FirebaseVisionImage firebaseImage = FirebaseVisionImage.fromFile(widget.imageFile);
+    final List<Face> detectedFaces = await faceDetector.processImage(firebaseImage);
     // faceDetector.close();
+    final List<Face> filteredFaces = [];
+    for (var face in detectedFaces) {
+      if (face.boundingBox.longestSide / face.boundingBox.shortestSide > 2) {
+        continue;
+      }
+      filteredFaces.add(face);
+    }
 
-  return detectedFaces;
+  return filteredFaces;
+  }
+
+  Future getIamgeSize() async {
+    final memoryImageSize = ImageSizeGetter.getSize(FileInput(widget.imageFile));
+    return memoryImageSize.width;
+    // return max(memoryImageSize.height,memoryImageSize.width );
   }
 
   Future loadImage() async {
+    print('image width is:');
     final data = await widget.imageFile.readAsBytes();
+    // final memoryImageSize = ImageSizeGetter.getSize(MemoryInput(data));
+    // final image = Image.memory(
+    //     data,
+    //     width: memoryImageSize.width.toDouble(),
+    //     height: memoryImageSize.height.toDouble(),
+    //   );
+    // print(memoryImageSize);
     return data;
   }
-
-
 }
 
 
