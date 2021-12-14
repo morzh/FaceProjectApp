b9a6cd2084115dbd58d5799442b39321af96e181
morzh <morzh80@mail.ru>
2021-12-14 12:19:57 +0300

    face attributes ranges added

--- lib/core/controllers/face_data_controller.dart
+++ lib/core/controllers/face_data_controller.dart
@@ -4,6 +4,31 @@ import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+class FaceAttributesRanges {
+  static const Map<String, num> min = {
+    'Gender': 0,
+    'Glasses': 0,
+    'Yaw': -20.0,
+    'Pitch': -20.0,
+    'Baldness': 0.0,
+    'Beard': 0.0,
+    'Age': 0,
+    'Expression': 0.0
+  };
+
+  static const Map<String, num> max = {
+    'Gender': 1,
+    'Glasses': 1,
+    'Yaw': 20.0,
+    'Pitch': 20.0,
+    'Baldness': 1.0,
+    'Beard': 1.0,
+    'Age': 65,
+    'Expression': 1.0
+  };
+}
+
+
 class FaceDataController extends GetxController {
   late final File croppedImage;
   final alignedImage = File('').obs;
